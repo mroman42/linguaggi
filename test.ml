914595302015ed7472eced9d9f7c48d0e424238a
M42 <mromang08@gmail.com>
2015-10-08 13:02:19 +0200

    Arrays

--- test.ml
+++ test.ml
@@ -5,7 +5,30 @@ let average a b =
 
 let my_ref = ref 0;; (* References (!) *)
 
-(* () is the unit *)
+let negation x = 
+  match x with
+    | true -> false
+    | false -> true
+  ;;
+
+let rec factorial n =
+  match n with
+    | 0 -> 1
+    | n -> n*(factorial (n-1))
+  ;;
+
+let rec map f list =
+  match list with
+    | [] -> []
+    | x :: xs -> (f x) :: (map f xs) 
+;;
+
+(* There are arrays and records*)
+let arrayexp = [|1;2;3;5|];;
+type person = {name: string; mutable age:int};;
+type tree = Nothing | Node of tree;;  
+
+(* () is the unit z l*)
 let main () = print_string ("Hello World!\n");;
 main ();;
 
