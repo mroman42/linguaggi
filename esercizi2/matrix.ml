a8058d573a4a86fefe15fb4de8dfa63f5b024965
M42 <mromang08@gmail.com>
2015-10-25 18:09:05 +0100

    First attempt of a Matrix module

--- esercizi2/matrix.ml
+++ esercizi2/matrix.ml
@@ -1,13 +1,33 @@
 module Matrix : sig
-  type matrix = int array array
-  val eq : matrix -> matrix -> bool
-  val copy : matrix -> matrix
-  val add : matrix -> matrix -> matrix
-  val scalar : float -> matrix -> matrix
+  type 'a matrix
+  val eq : 'a matrix -> 'a matrix -> bool
+  (*val copy : 'a matrix -> 'a matrix*)
+  val combine : 'a matrix -> 'b matrix -> ('a*'b) matrix
+  val add : int matrix -> int matrix -> int matrix
+  (*val scalar : float -> matrix -> matrix
   val prod : matrix -> matrix -> matrix
   val transposition : matrix -> matrix
-  val norm : matrix -> int
-end =
+  val norm : matrix -> int*)
+end with type 'a matrix = 'a array array =
 struct
-  
+  type 'a matrix = 'a array array
+  let eq = (=);;
+  (*let copy = Array.map (Array.copy);;*)
+
+    
+  let combine a b =
+    if (Array.length a == Array.length b) then
+      let combinearray a b = if Array.length a == Array.length b then
+                               Array.mapi (fun i x -> (x,b.(i))) (Array.copy a)
+                             else (raise (Failure "Invalid dimensions"))
+      in
+      Array.mapi (fun i x -> combinearray x (b.(i))) (Array.copy a)
+    else (raise (Failure "Invalid dimensions"))
+  ;;
+
+  let add a b = 
+    Array.map (Array.map (fun (x,y) -> x+y)) (combine a b)
+  ;;
+
+
 end
