7ad5f794a7d3a6542f5a0b32e0ce914f697371f6
M42 <mromang08@gmail.com>
2015-10-26 12:25:15 +0100

    Arithmetic functions

--- ocaml/matrix.ml
+++ ocaml/matrix.ml
@@ -6,32 +6,47 @@ module Matrix : sig
   val add : int matrix -> int matrix -> int matrix
   val map : ('a -> 'b) -> 'a matrix -> 'b matrix
   val dimensions : 'a matrix -> (int * int)
-  (*val scalar : float -> matrix -> matrix
-  val prod : matrix -> matrix -> matrix
+  val make : (int -> int -> 'a) -> int -> int -> 'a matrix
+  val scalar : int -> int matrix -> int matrix
+  (*val prod : matrix -> matrix -> matrix
   val transposition : matrix -> matrix
   val norm : matrix -> int*)
 end with type 'a matrix = 'a array array =
 struct
   type 'a matrix = 'a array array
+
   let eq = (=);;
 
   (*let copy = Array.map (Array.copy);;*)
 
+  let make f h w = 
+    let m = (Array.make_matrix h w (f 0 0)) in
+    for i=0 to h-1 do
+      for j=0 to w-1 do
+        m.(i).(j) <- f i j
+      done
+    done;
+    m
+  ;;
+
   let map f = Array.map (Array.map f);;
+
   let dimensions m = (Array.length m, Array.length m.(1));;
   
   let combine a b =
-    if (Matrix.dimensions a != Matrix.dimensions b) then
+    if not (dimensions a = dimensions b) then
       raise (Failure "Invalid dimensions")
-    else
-      let combinearray a b = if Array.length a == Array.length b then
-                               Array.mapi (fun i x -> (x,b.(i))) (Array.copy a)
-                             else (raise (Failure "Invalid dimensions"))
-      in
-      Array.mapi (fun i x -> combinearray x (b.(i))) (Array.copy a)
+    else 
+      let (h,w) = dimensions a in
+      make (fun i j -> (a.(i).(j), b.(i).(j))) h w
   ;;
 
-  let add a b = 
-    Matrix.map (fun (x,y) -> x+y) (combine a b)
-  ;;
+
+  (* Aritmetic functions *)
+  let add a b = map (fun (x,y) -> x+y) (combine a b);;
+  let scalar a m = map (fun x -> a*x) m;;
+  let prodm a b = map (fun (x,y) -> x*y) (combine a b);;
+  let prod a b = 
+    let sum = List.fold_left (+) 0 in 
+    make (fun (i,j) -> sum [])
 end
