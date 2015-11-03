2336cd220ce5087c21fc5249561d9abd833cdb3a
M42 <mromang08@gmail.com>
2015-11-03 18:26:28 +0100

    Nats as a module

--- ocaml/nats.ml
+++ ocaml/nats.ml
@@ -1,20 +1,50 @@
 (* Natural numbers *)
-type nat = Z | S of nat;;
+module type NaturalStructure =
+  sig 
+    type nat
+    val zero : nat
+    val succ : nat -> nat
+    val pred : nat -> nat
+  end;;
 
-let succ n = S n;;
+module Naturals : NaturalStructure =
+  struct
+    type nat = Z | S of nat;;
+    let succ n = S n;;
+    let zero = Z;;
+     
+    let pred =
+      function Z -> Z
+             | S n -> n
+    ;;
 
-let rec add a = 
-  function Z -> a 
-         | S b -> S (add a b)
-;;
 
-let rec prod a = 
-  function Z   -> Z
-         | S b -> add (prod a b) a
-;;
 
-let rec toint = 
-  function Z   -> 0
-         | S n -> 1 + (toint n)
-;;
+    let rec toint = 
+      function Z   -> 0
+             | S n -> 1 + (toint n)
+    ;;
+end;;
 
+module IntNats : NaturalStructure 
+       with type nat = int 
+= 
+  struct
+    type nat = int;;
+    let succ = fun x -> x+1;;
+    let zero = 0;;
+    let pred = 
+      function 0 -> 0
+             | n -> n-1
+    ;;
+end;;
+
+
+module NaturalArithmetic (Nats : NaturalStructure) =
+  struct 
+    let rec add a b = if (b == Nats.zero)
+                      then a 
+                      else Nats.succ (add a (Nats.pred b));;
+  end;;
+
+module IntArithm = NaturalArithmetic (IntNats);;
