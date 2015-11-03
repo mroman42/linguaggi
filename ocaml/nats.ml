5e93d707b4fa4e0bd27c3830a8f8a06e504e1166
M42 <mromang08@gmail.com>
2015-11-03 17:52:25 +0100

    Some simple definitions

--- ocaml/nats.ml
+++ ocaml/nats.ml
@@ -1,1 +1,20 @@
 (* Natural numbers *)
+type nat = Z | S of nat;;
+
+let succ n = S n;;
+
+let rec add a = 
+  function Z -> a 
+         | S b -> S (add a b)
+;;
+
+let rec prod a = 
+  function Z   -> Z
+         | S b -> add (prod a b) a
+;;
+
+let rec toint = 
+  function Z   -> 0
+         | S n -> 1 + (toint n)
+;;
+
