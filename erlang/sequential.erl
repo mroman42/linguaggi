8fedf167b22f2d1f0ba0f8b2c4e65449208011cd
M42 <mromang08@gmail.com>
2015-11-12 14:30:07 +0100

    Prime factors in erlang

--- erlang/sequential.erl
+++ erlang/sequential.erl
@@ -1,5 +1,5 @@
 -module(sequential).
--export([is_palindrome/1]).
+-export([is_palindrome/1, factor/1]).
 
 % Palindrome.
 % Checks if the string can be read the same way in either directions, in spite 
@@ -8,4 +8,11 @@ is_palindrome(Str) ->
     FilteredStr = re:replace(Str, "[^A-Za-z]", "", [global,{return, list}]),
     LowerCaseStr = string:to_lower(FilteredStr),
     string:equal(LowerCaseStr, lists:reverse(LowerCaseStr)).
-        
+
+% Factors
+% Given a number, computes its prime factors.
+factor(N) when N>0 -> factor(N,[],2).
+
+factor(N,R,I) when (I*I > N) -> [N|R];                                
+factor(N,R,I) when ((N rem I) =:= 0) -> factor(N div I, [I|R], I);
+factor(N,R,I) -> factor(N,R,I+1).
