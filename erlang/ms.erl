fc53f96ee3043b382c86f078e53dc7c1f4986165
M42 <mromang08@gmail.com>
2015-11-30 11:16:06 +0100

    Comments master-slave

--- erlang/ms.erl
+++ erlang/ms.erl
@@ -1,14 +1,23 @@
 -module(ms).
 -export([start/1, to_slave/2]).
 
+% Registers the master at start
 start(N) ->
     register(master, spawn(fun() -> masterpr(N) end)),
     ok.
 
+
+% Master main function
+masterpr(N) ->
+    process_flag(trap_exit, true),
+    master_spawn(N),
+    master_wait().
+
+% Sends a message to the master, in order to send it to the slave
 to_slave(Text, M) ->
     master ! {msg, M, Text}.
 
-
+% Spawn slave processes
 master_spawn(N) ->
     lists:map(fun(X) ->
                       register(
@@ -18,6 +27,8 @@ master_spawn(N) ->
               lists:seq(1,N)
              ).
 
+
+% Wait for a message to the slaves
 master_wait() ->
     receive
         {msg, M, Text} -> 
@@ -29,22 +40,20 @@ master_wait() ->
             io:format("Slave process "++integer_to_list(M)++" restarted.")
     end.
 
-masterpr(N) ->
-    process_flag(trap_exit, true),
-    master_spawn(N),
-    master_wait().
 
+% Slave main function. It is linked to the master.
 slavepr(X) ->
     link(whereis(master)),
     slave_wait(X).
 
+% Wait for a message from the master
 slave_wait(X) ->
     receive
         {msg, die} ->
             io:format("slave" ++ integer_to_list(X) ++  " dies."),
             exit({X,normal});
         {msg, Text} ->
-            io:format("slave" ++ integer_to_list(X) ++  "prints: " ++ Text),
+            io:format("slave" ++ integer_to_list(X) ++  " prints: " ++ Text ++ "\n"),
             slave_wait(X)
     end.
 
