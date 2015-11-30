856fb6d4ed98a782e96706053cc1b30a442c6f68
M42 <mromang08@gmail.com>
2015-11-30 13:33:18 +0100

    Comments counting

--- erlang/counting.erl
+++ erlang/counting.erl
@@ -1,11 +1,12 @@
 -module(counting).
 -export([start/0, stop/0, tot/0, echo/1, reverse/1, sort/1]).
 
+% Start the server
 start() ->
     register(server, spawn(fun() -> loop({count,0,0,0}) end)),
     ok.
 
-
+% Server main function
 loop({count,E,R,S}) ->
     receive
         {echo,Text} -> io:format(Text++"\n"),
@@ -18,19 +19,20 @@ loop({count,E,R,S}) ->
                  loop({count,E,R,S})
     end.
 
-
+% Functions calling the server
 tot() -> server ! {tot}.
 echo(Text) -> server ! {echo,Text}.
 reverse(Text) -> server ! {reverse,Text}.
 sort(Text) -> server ! {sort,Text}.
 
-
+% Auxiliary format function
 print(E,R,S) ->
     io:format("Counter:\n"++
                   "echo: "++integer_to_list(E)++"\n"++
                   "reverse: "++integer_to_list(R)++"\n"++
                   "sort: "++integer_to_list(S)++"\n").
-    
+
+% Stops the server. Unregisters the process
 stop() ->
     exit(whereis(server), kill),
     ok.
