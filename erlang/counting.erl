7d8eed5ca2aa7e243d327a298090f47713e6fd3b
M42 <mromang08@gmail.com>
2015-11-30 12:15:44 +0100

    Counting final version

--- erlang/counting.erl
+++ erlang/counting.erl
@@ -5,26 +5,32 @@ start() ->
     register(server, spawn(fun() -> loop({count,0,0,0}) end)),
     ok.
 
+
 loop({count,E,R,S}) ->
     receive
-        {tot} -> io:format("Counter:\n"++
-                               "echo: "++integer_to_list(E)++"\n"++
-                               "reverse: "++integer_to_list(R)++"\n"++
-                               "sort: "++integer_to_list(S)++"\n"),
-                 loop({count,E,R,S});
         {echo,Text} -> io:format(Text++"\n"),
                        loop({count,E+1,R,S});
         {reverse,Text} -> io:format(lists:reverse(Text)++"\n"), 
                           loop({count,E,R+1,S});
         {sort,Text} -> io:format(lists:sort(Text)++"\n"), 
-                       loop({count,E,R,S+1})
+                       loop({count,E,R,S+1});
+        {tot} -> print(E,R,S),
+                 loop({count,E,R,S})
     end.
+
 
 tot() -> server ! {tot}.
 echo(Text) -> server ! {echo,Text}.
 reverse(Text) -> server ! {reverse,Text}.
 sort(Text) -> server ! {sort,Text}.
 
+
+print(E,R,S) ->
+    io:format("Counter:\n"++
+                  "echo: "++integer_to_list(E)++"\n"++
+                  "reverse: "++integer_to_list(R)++"\n"++
+                  "sort: "++integer_to_list(S)++"\n").
+    
 stop() ->
     exit(whereis(server), kill),
     ok.
