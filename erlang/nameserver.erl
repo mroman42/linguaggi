10913438362ad06850d5c215f4fd3b8ab352e728
M42 <mromang08@gmail.com>
2015-12-01 23:01:51 +0100

    Comments the name server

--- erlang/nameserver.erl
+++ erlang/nameserver.erl
@@ -1,16 +1,22 @@
 -module(nameserver).
 -export([start/0,store/2,lookup/1]).
 
+% User interface
 start() -> register(kvs, spawn(fun() -> loop() end)).
 store(Key,Value) -> query({store,Key,Value}).
 lookup(Key) -> query({lookup,Key}).
 
+% A function that makes a query to the server and waits for a response.
+% In this protocol, it is necessary to send your PID to the server
 query(Q) ->
     kvs ! {self(), Q},
     receive
         {kvs,Reply} -> Reply
     end.
 
+% Server main function
+% The server uses the process dictionary to store the given keys.
+% Note that this use is discouraged in Erlang. (!)
 loop() ->
     receive
         {From, {store,Key,Value}} ->
