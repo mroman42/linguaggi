f88151b851ccf094b81bb863174eb35bbc8c582c
M42 <mromang08@gmail.com>
2016-01-16 17:40:17 +0100

    Basic algebra in Scala

--- scala/algebra.scala
+++ scala/algebra.scala
@@ -2,10 +2,24 @@ trait Monoid[T] {
   def zero: T
   def op: (T,T) => T
 
-  def mconcat(list:List[T]) : T = list.fold(zero) {(a,b)=>op(a,b)}
+  def mconcat(list:List[T]) : T = list.fold(zero) {op}
 }
 
-object PlusMonoid extends Monoid[Int] {
+trait Group[T] extends Monoid[T] {
+  def id : T = zero
+  def opt : (T,T) => T = op
+  def inv : T => T
+
+  def prod : List[T] => T = mconcat
+}
+
+object Plus extends Group[Int] {
   def zero = 0
   def op = (_+_)
+  def inv = (-_)
+}
+
+object Mult extends Monoid[Int] {
+  def zero = 1
+  def op = (_*_)
 }
